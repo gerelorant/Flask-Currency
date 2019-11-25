5fece1b45acd6ec6833638e0fb01d2895ffed57b
Gere Lóránt <gerelorant@gmail.com>
2019-11-25 09:59:53 +0100

    License added

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='FlaskCurrency',
     version='v1.0',
     packages=['flask_currency'],
-    url='',
-    license='',
+    url='https://github.com/gerelorant/Flask-Currency',
+    license='MIT',
     author='Gere Lóránt',
     author_email='gerelorant@gmail.com',
     description='Currency handling extension for Flask and SQLAlchemy'
